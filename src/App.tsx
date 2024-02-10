f98bb593cfb654eaf21f144bf7647d1df1836bae
HoussemDAAS <houssemdaas2@gmail.com>
2024-02-10 16:32:56 +0100

    test deploy 3

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} />52488485
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
